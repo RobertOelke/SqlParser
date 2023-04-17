0dbdbe584c9aa70df6384925cbcc7cc11e361271
Robert Oelke <robert.oelke90@web.de>
2023-04-17 20:18:52 +0200

    Tests for keywords added.

--- SqlParser.Tests/LexerTests.cs
+++ SqlParser.Tests/LexerTests.cs
@@ -1,10 +1,28 @@
 namespace SqlParser.Tests;
 
+using SqlParser;
+
 public class LexerTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [MemberData(nameof(GetKeywords))]
+    public void KeywordTests(string testString, SyntaxKind expectedKeyword)
     {
+        var lexer = new Lexer(testString);
 
+        var parsedToken = lexer.NextToken();
+
+        Assert.Equal(expectedKeyword, parsedToken.Kind);
+    }
+
+    public static IEnumerable<object[]> GetKeywords()
+    {
+        yield return new object[] { "SELECT", SyntaxKind.SelectToken };
+        yield return new object[] { "select", SyntaxKind.SelectToken };
+        yield return new object[] { "sELEct", SyntaxKind.SelectToken };
+        yield return new object[] { "sELEct123", SyntaxKind.LiteralToken };
+
+        yield return new object[] { "WHERE", SyntaxKind.WhereToken };
+        yield return new object[] { "FROM", SyntaxKind.FromToken };
     }
 }
