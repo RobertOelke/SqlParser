e6f2096e6e10d1c771cf053b1a8e5e3dda130ddc
Robert Oelke <robert.oelke90@web.de>
2023-04-29 11:28:36 +0200

    Added repl.

---
+++ SqlParser.Repl/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace SqlParser.Repl;
+
+public static class Program
+{
+    public static void Main()
+    {
+        bool cancelRequested = false;
+
+        while (!cancelRequested)
+        {
+            Console.Write("> ");
+
+            var line = Console.ReadLine();
+
+            switch (line)
+            {
+                case "help":
+                    PrintHelp();
+                    break;
+                case "clear":
+                    HandleClear();
+                    break;
+                case "exit":
+                    cancelRequested = true;
+                    break;
+                default:
+                    PrintUnkownCommand(line);
+                    break;
+            }
+
+            if (cancelRequested)
+                break;
+        }
+    }
+
+    private static void PrintHelp()
+    {
+        Console.WriteLine("| 'help'  => lists all commands.");
+        Console.WriteLine("| 'clear' => clears the console.");
+        Console.WriteLine("| 'exit'  => exits the program.");
+    }
+
+    private static void HandleClear()
+    {
+        Console.Clear();
+    }
+
+    private static void PrintUnkownCommand(string? line)
+    {
+        Console.WriteLine($"| Unknown command '{line}'");
+        Console.WriteLine($"| Try 'help' to see all available commands");
+    }
+}
--- SqlParser/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿var lexer = new SqlParser.Tokenization.Lexer("SELECT A, B FROM C WHERE D = 2");
-
-while (lexer.NextToken() is var token
-    && token.Kind != SqlParser.Tokenization.SyntaxKind.EndOfFileToken)
-{
-    Console.WriteLine(token);
-}
